5c1f2e82cd891a1c500747006f6f142589138fce
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-08-18 12:28:42 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,57 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use dotenv::dotenv;
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn index(req: HttpRequest) -> &'static str {
+    println!("REQ: {req:?}");
+    "Hello world!"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // dotenv().expect(".env file not found");
+    dotenv().ok();
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()
+        .expect("PORT must be anumber");
+
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting HTTP server at http://localhost:{}", port);
+
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/index.html").to(|| async { "Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!" }))
+            .service(web::resource("/").to(index))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{body::to_bytes, dev::Service, http, test, Error};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_index() -> Result<(), Error> {
+        let app = App::new().route("/", web::get().to(index));
+        let app = test::init_service(app).await;
+
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = app.call(req).await?;
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = resp.into_body();
+        assert_eq!(to_bytes(response_body).await?, r##"Hello world!"##);
+
+        Ok(())
+    }
 }
